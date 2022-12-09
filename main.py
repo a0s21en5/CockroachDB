86be14f28298d5c8e8f34ce30db71b7ac8e0d755
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-09 06:12:56 +0000

    Update

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from plyer import notification
 import time
 
 if __name__ == '__main__':
-    notification.notify(
-        title="*** Take Rest ***",
-        message="Rest is Vital for better Mental Health"
-        app_icon="")
+	while True:
+		notification.notify(title='*** Take Rest ***', 
+					message='Rest is Vital for better Mental Health',
+					app_icon='C:/Users/Voldemort/Desktop/image.ico',
+					timeout=5)
+		time.sleep(10)
+
+#pythonw main.py
