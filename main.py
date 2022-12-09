c5369e65cd77d7bebd00a78d83f53dc437afd541
Ashish Sahu <86140629+a0s21en5@users.noreply.github.com>
2022-12-09 05:58:14 +0000

    Update

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from plyer import notification
-import time 
+import time
 
 if __name__ == '__main__':
-	notification.notify(
-			title = "*** Take Rest ***",
-			message = "Rest is Vital for better Mental Health"
-			app_icon ="")
+    notification.notify(
+        title="*** Take Rest ***",
+        message="Rest is Vital for better Mental Health"
+        app_icon="")
