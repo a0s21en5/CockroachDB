b6de130b8a4d5b84d69abaaa3763ab2ccfc66169
Ashish Sahu <ashsahu9713@gmail.com>
2022-12-09 11:25:56 +0530

    Update

---
+++ main.py
@@ -0,0 +1,8 @@
+from plyer import notification
+import time 
+
+if __name__ == '__main__':
+	notification.notify(
+			title = "*** Take Rest ***",
+			message = "Rest is Vital for better Mental Health"
+			app_icon ="")
